1dc3627f52d1ddf0a467894ddd508dd51ae57688
Adriaan Odendaal <adriaan.odendaal1@gmail.com>
2019-08-12 23:08:25 -0500

    console plugin remove

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -535,7 +535,6 @@ module.exports.metadata =
 {
     "cordova-plugin-battery-status": "1.1.2",
     "cordova-plugin-camera": "2.1.1",
-    "cordova-plugin-console": "1.0.7",
     "cordova-plugin-contacts": "2.0.1",
     "cordova-plugin-device": "1.1.7",
     "cordova-plugin-device-motion": "1.2.5",
